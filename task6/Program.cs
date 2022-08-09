6915d18f0c376a443bded232379f2fc5ba637b6d
OlegSam2 <doorsmania@mail.ru>
2022-08-09 20:22:52 +0300

    add homework task6 (even or odd)

---
+++ task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 0) 
+{
+    Console.WriteLine(numberA + " - четное число");
+}
+else
+{
+    Console.WriteLine(numberA + " - нечетное число");
+}
